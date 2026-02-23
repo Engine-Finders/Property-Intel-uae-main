13f48f64b3bfde68d28f27adbdf47cdbb4f64f22
hashirafridi <hashirafridi474747@gmail.com>
2026-02-23 22:48:41 +0500

    deployment issuess

--- src/app/(public)/not-found/page.js
+++ src/app/(public)/not-found/page.js
@@ -1,14 +1,8 @@
-import { useLocation } from "react-router-dom";
+"use client";
 import { useEffect } from "react";
 import Link from "next/link";
 
 const NotFound = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    console.error("404 Error: User attempted to access non-existent route:", location.pathname);
-  }, [location.pathname]);
-
   return (
     <div className="flex min-h-screen items-center justify-center bg-muted">
       <div className="text-center">
